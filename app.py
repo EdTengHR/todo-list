4d28b5fe51b2a15558498a2ae8b0956c1baefcc2
EdTengHR <edmundteng988@gmail.com>
2022-06-29 18:09:20 +0800

    implemented all endpoints

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify
 import json
 from flask_pymongo import PyMongo
 import os
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = 'mongodb://localhost:27017/flaskdb'
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 mongo = PyMongo(app)
 db = mongo.db
@@ -19,117 +20,69 @@ def index():
 def show():
     toDoList = db.todo.find()
 
-    # item = {}
-    # data = []
-    # for todo in toDoList:
-    #     item = {
-    #         'id': str(todo['_id']),
-    #         'todo': todo['todo'],
-    #         'marked': todo['marked']
-    #     }
+    item = {}
+    data = []
+    for todo in toDoList:
+        item = {
+            '_id': str(todo['_id']),
+            'id' : todo['id'],
+            'todo': todo['todo'],
+            'marked': todo['marked']
+        }
 
-    #     data.append(item)
+        data.append(item)
 
-    # return jsonify(
-    #     status=True,
-    #     data=data
-    # )
+    return jsonify(
+        data=data
+    )
 
-    return jsonify([todo for todo in toDoList])
-
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/add', methods=['GET'])
 def add():
-    if request.method == 'GET':
-        data_id = request.args.get('id')
-        data = request.args.get('todo')
+    data_id = request.args.get('id')
+    data = request.args.get('todo')
 
-        toAdd = {
-            '_id': data_id,
-            'todo': data,
-            'marked': False,
-        }
+    toAdd = {
+        'id': data_id,
+        'todo': data,
+        'marked': False,
+    }
 
-        db.todo.insert_one(toAdd)
+    db.todo.insert_one(toAdd)
 
-        return jsonify(
-            status=True,
-            message='Item ' + data + ' added successfully!'
-        ), 201
+    return jsonify(
+        status=True,
+        message='Item ' + data + ' added successfully!'
+    ), 201
 
-    else: 
-        data = request.get_json(force=True)
-
-        toAdd = {
-            'todo': data['todo'],
-            'marked': False,
-        }
-
-        db.todo.insert_one(toAdd)
-
-        return jsonify(
-            status=True,
-            message='Item ' + data['todo'] + ' added successfully!'
-        ), 201
-
-@app.route('/delete', methods=['GET', 'DELETE'])
+@app.route('/delete', methods=['GET'])
 def delete():
-    if request.method == 'GET':
-        data = request.args.get('id')
+    _id = request.args.get('_id')
 
-        toDelete = {
-            '_id': data
-        }
+    toDelete = {
+        '_id': ObjectId(_id)
+    }
 
-        db.todo.delete_one(toDelete)
+    result = db.todo.delete_one(toDelete)
 
-        return jsonify(
-            status=True,
-            message='Item ' + data + ' deleted successfully!'
-        ), 200
+    return jsonify(
+        status=True,
+        message=str(result.deleted_count) + ' document deleted'
+    ), 200
 
-    else:
-        data = request.get_json(force=True)
-
-        toDelete = {
-            '_id': data['id']
-        }
-
-        db.todo.delete_one(toDelete)
-
-        return jsonify(
-            status=True,
-            message='Item ' + data['id'] + ' deleted successfully!'
-        ), 200
-
-@app.route('/mark-complete', methods=['GET', 'PUT'])
+@app.route('/mark-complete', methods=['GET'])
 def markComplete():
-    if request.method == 'GET':
-        data = request.args.get('id')
+    _id = request.args.get('_id')
 
-        toMark = {
-            '_id': data
-        }
+    toMark = {
+        '_id': ObjectId(_id)
+    }
 
-        db.todo.update_one(toMark, { "$set": { "marked": "true" } })
+    result = db.todo.update_one(toMark, { "$set": { "marked": True } })
 
-        return jsonify(
-            status=True,
-            message='Item ' + data + ' marked as completed! (GET)'
-        ), 200
-
-    else:
-        data = request.get_json(force=True)
-
-        toMark = {
-            '_id': data['id']
-        }
-
-        db.todo.update_one(toMark, { "$set": { "marked": True } })
-
-        return jsonify(
-            status=True,
-            message='Item ' + data['id'] + ' marked as completed!'
-        ), 200
+    return jsonify(
+        status=True,
+        message=str(result.modified_count) + ' documents modified'
+    ), 200
 
 if __name__ == '__main__':
     # Default port is 5000 on local host
